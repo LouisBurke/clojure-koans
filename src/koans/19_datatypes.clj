a9e663b652eb5a470588c67df703853d00465584
Louis Burke <louis.j.burke@gmail.com>
2016-11-16 14:20:30 +0000

    19_datatypes

--- src/koans/19_datatypes.clj
+++ src/koans/19_datatypes.clj
@@ -1,14 +1,15 @@
 (ns koans.19-datatypes
   (:require [koan-engine.core :refer :all]))
 
-(defrecord Nobel [prize])
+(defrecord Nobel [prize]) ; meets a map contract, in other words
+                                        ; can be treated like a map.
 (deftype Pulitzer [prize])
 
-(defprotocol Award
+(defprotocol Award ; sort of like an interface.
   (present [this recipient]))
 
 (defrecord Oscar [category]
-  Award
+  Award ; should implement the Award protocol see interface above.
   (present [this recipient]
     (print (str "Congratulations on your "
                 (:category this) " Oscar, "
@@ -18,28 +19,31 @@
 (deftype Razzie [category]
   Award
   (present [this recipient]
-    __))
+    (print (str "You're really the "
+                (.category this) ", "
+                recipient
+                "... sorry."))))
 
 (meditations
   "Holding records is meaningful only when the record is worthy of you"
-  (= __ (.prize (Nobel. "peace")))
+  (= "peace" (.prize (Nobel. "peace")))
 
   "Types are quite similar"
-  (= __ (.prize (Pulitzer. "literature")))
+  (= "literature" (.prize (Pulitzer. "literature")))
 
   "Records may be treated like maps"
-  (= __ (:prize (Nobel. "physics")))
+  (= "physics" (:prize (Nobel. "physics")))
 
   "While types may not"
-  (= __ (:prize (Pulitzer. "poetry")))
+  (= nil (:prize (Pulitzer. "poetry")))
 
   "Further study reveals why"
-  (= __
+  (= [true false]
      (map map? [(Nobel. "chemistry")
                 (Pulitzer. "music")]))
 
   "Either sort of datatype can define methods in a protocol"
-  (= __
+  (="Congratulations on your Best Picture Oscar, Evil Alien Conquerors!"
      (with-out-str (present (Oscar. "Best Picture") "Evil Alien Conquerors")))
 
   "Surely we can implement our own by now"
